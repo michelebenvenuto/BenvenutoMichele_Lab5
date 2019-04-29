d071450aa734588514b6f83c77edb3078db2aa96
michelebenveuto <35434145+michelebenveuto@users.noreply.github.com>
2019-04-29 13:59:59 -0600

    final version

---
+++ BenvenutoMichele_lab4/Assets/myScripts/ball.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour
+{
+    private Rigidbody rb;
+    private int force = 20;
+    private AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        //Adds a force
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if(Physics.Raycast(myRay, out hitInfo))
+        {
+            rb.AddForce(-hitInfo.normal*force, ForceMode.Impulse);
+        }
+        //Plays the sound
+        if (audio) {
+            audio.Play();
+        }
+    }
+    private void OnMouseOver()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+    }
+    private void OnMouseExit()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.white;
+    }
+}
---
+++ BenvenutoMichele_lab4/Assets/myScripts/player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player : MonoBehaviour
+{
+    public Text targetCounter;
+    public AudioClip targetHit;
+    private int hitTargets;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray aim = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+        RaycastHit hitInfo;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(aim, out hitInfo))
+            {
+                if (hitInfo.collider.CompareTag("target"))
+                {
+                    hitTargets += 1;
+                    targetCounter.text = hitTargets.ToString();
+                    Destroy(hitInfo.collider.gameObject);
+                    audioSource.PlayOneShot(targetHit);
+                }
+            }
+        }
+    }
+}
